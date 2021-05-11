2fad1934a228b1b4e666edc0716dcf1d0f6db1e4
Ricardo Gusi <ricardogusi@hotmail.com>
2021-05-11 16:12:07 -0300

    First commit

---
+++ src/main/java/gusi/springframework/projetofinancas/ProjetoFinancasApplication.java
@@ -0,0 +1,13 @@
+package gusi.springframework.projetofinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gusi/springframework/projetofinancas/ProjetoFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package gusi.springframework.projetofinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
